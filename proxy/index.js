71bf65454bd765a266a210e32d5312a1d9aa83c0
Christoffer <cgranzell@hotmail.com>
2023-04-12 15:22:10 +0200

    tesat cors

--- proxy/index.js
+++ proxy/index.js
@@ -15,7 +15,7 @@ const API_SERVICE_URL = "https://gohealth.hiq.se";
 // Add headers before the routes are defined
 app.use(function (_req, res, next) {
   // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Origin', 'https://playful-gumption-8420ca.netlify.app/');
 
 
